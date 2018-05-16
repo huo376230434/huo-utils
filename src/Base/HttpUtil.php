312d1c104cc896f7f8e99444d8dc76cd1033f2a0
huo <376230434@qq.com>
2018-05-16 17:32:34 +0800

    添加HTTPUtil

---
+++ src/Base/HttpUtil.php
@@ -0,0 +1,37 @@
+<?php
+namespace HuoUtils\Base;
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/17
+ * Time: 16:35
+ */
+
+class HttpUtil{
+
+
+//    连接参数字符串
+    public static function concatParams($params,$out_array = []) {
+        ksort($params);
+        $pairs = array();
+        foreach($params as $key=>$val) {
+            if(in_array($key,$out_array)){
+                continue;
+            }
+            array_push($pairs, $key . '=' . $val);
+        }
+        return join('&', $pairs);
+    }
+
+//参数数组拼接成
+
+    public static function buildGetParamsUrl($url,$params)
+    {
+        $new_url = $url . "?";
+        $new_url .= self::concatParams($params);
+        return $new_url;
+
+    }
+
+}

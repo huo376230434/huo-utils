4fdcabf5cf048eaa92688043d58e786b9891ce61
huo <376230434@qq.com>
2018-06-25 09:54:28 +0800

    添加 文件夹删除

--- src/Base/FileUtil.php
+++ src/Base/FileUtil.php
@@ -64,6 +64,27 @@ class FileUtil{
     }
 
 
+    public static function rmDir($dir_name,$echo=true)
+    {
+        if ( $handle = opendir( "$dir_name" ) ) {
+            while ( false !== ( $item = readdir( $handle ) ) ) {
+                if ( $item != "." && $item != ".." ) {
+                    if ( is_dir( "$dir_name/$item" ) ) {
+                        self::rmTotalDir("$dir_name/$item" );
+                    } else {
+                        if( unlink( "$dir_name/$item"  && $echo)  ) {
+                           echo "成功删除文件： $dir_name/$item\n";
+                        }
+                    }
+                }
+            }
+            closedir( $handle );
+            if( rmdir( $dir_name )   && $echo )echo "成功删除目录： $dir_name\n";
+        }
+
+    }
+
+
 
 
 }
